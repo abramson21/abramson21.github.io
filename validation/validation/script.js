cd5c22c56679341a2d5689cb2722155cf99e7924
Andrey Nikolaev <abramson21@mail.ru>
2019-10-08 10:44:38 +0300

    My first commit

---
+++ validation/validation/script.js
@@ -0,0 +1,18 @@
+/*register.addEventListener('input', (event) => {
+    const nameValue = document.forms.register.elements.name.value;
+    const newnameValue = nameValue.substr(0,1);
+    const reg = /[А-Я]/g;
+    const str = newnameValue.match(reg);
+    console.log(str);
+    const joinSTR = str.join('');
+
+    if (newnameValue != joinSTR ||  зззз === null){
+        console.log('НЕ Чётко');
+    }
+    else {console.log('чётко')}
+    //const reg ='^[а-яА-ЯёЁ\s]'
+    //nameValue.match(reg);
+    //const name = document.querySelector('');
+})
+*/
+
